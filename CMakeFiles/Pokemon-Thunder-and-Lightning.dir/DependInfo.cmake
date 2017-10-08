1f8ae3c2e47ced90fb1b89154bf9590fbc0be702
Blaze349 <blaze349@protonmail.com>
2017-10-08 18:17:19 +1030

    Now using CMAKE cross-platform build system

---
+++ CMakeFiles/Pokemon-Thunder-and-Lightning.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/StoneofHelp/Programming/Pokemon-Clone/src/AnimatedSprite.cpp" "/Users/StoneofHelp/Programming/Pokemon-Clone/CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/AnimatedSprite.cpp.o"
+  "/Users/StoneofHelp/Programming/Pokemon-Clone/src/Animation.cpp" "/Users/StoneofHelp/Programming/Pokemon-Clone/CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/Animation.cpp.o"
+  "/Users/StoneofHelp/Programming/Pokemon-Clone/src/game.cpp" "/Users/StoneofHelp/Programming/Pokemon-Clone/CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/game.cpp.o"
+  "/Users/StoneofHelp/Programming/Pokemon-Clone/src/hero.cpp" "/Users/StoneofHelp/Programming/Pokemon-Clone/CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/hero.cpp.o"
+  "/Users/StoneofHelp/Programming/Pokemon-Clone/src/main.cpp" "/Users/StoneofHelp/Programming/Pokemon-Clone/CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/main.cpp.o"
+  "/Users/StoneofHelp/Programming/Pokemon-Clone/src/playing.cpp" "/Users/StoneofHelp/Programming/Pokemon-Clone/CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/playing.cpp.o"
+  "/Users/StoneofHelp/Programming/Pokemon-Clone/src/pokemon.cpp" "/Users/StoneofHelp/Programming/Pokemon-Clone/CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/pokemon.cpp.o"
+  "/Users/StoneofHelp/Programming/Pokemon-Clone/src/splashScreen.cpp" "/Users/StoneofHelp/Programming/Pokemon-Clone/CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/splashScreen.cpp.o"
+  "/Users/StoneofHelp/Programming/Pokemon-Clone/src/util.cpp" "/Users/StoneofHelp/Programming/Pokemon-Clone/CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "./bin"
+  "./include"
+  "/Library/Frameworks/SFML.framework"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Pokemon-Thunder-and-Lightning.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/AnimatedSprite.cpp.o"
+  "CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/Animation.cpp.o"
+  "CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/game.cpp.o"
+  "CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/hero.cpp.o"
+  "CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/main.cpp.o"
+  "CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/playing.cpp.o"
+  "CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/pokemon.cpp.o"
+  "CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/splashScreen.cpp.o"
+  "CMakeFiles/Pokemon-Thunder-and-Lightning.dir/src/util.cpp.o"
+  "Pokemon-Thunder-and-Lightning.pdb"
+  "Pokemon-Thunder-and-Lightning"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pokemon-Thunder-and-Lightning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
