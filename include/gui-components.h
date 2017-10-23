0c5865eeaa060368d4e69a6effbfe90b82335d32
Blaze349 <blaze349@protonmail.com>
2017-10-23 15:49:40 +1030

    DELETE USELESS CLASSES -ok that was fun

--- include/gui-components.h
+++ include/gui-components.h
@@ -1,21 +1,5 @@
 #ifndef GUI_COMPONENTS
 #define GUI_COMPONENTS
 
-void typewriterTextEffect() {
-
-}
-
-//reminder: use composition
-//reminder: game will be played almost entirely through keyboard
-
-class TextBox {
-
-};
-
-class ContextMenu {};
-
-
-
-
 
 #endif
