42aaf9bf5a516967c675d66f286cac2c13c7b357
Blaze349 <blaze349@protonmail.com>
2017-12-09 16:37:43 +1030

    Animation with normal skin as well

--- src/hero.cpp
+++ src/hero.cpp
@@ -18,56 +18,84 @@ void Hero::handleAnimations(AnimatedSprite& sprite) {
 
 }
 
+void setSpriteSheet(AnimationSet& animSet, sf::Texture& tex) {
+    animSet.up.setSpriteSheet(tex);
+    animSet.down.setSpriteSheet(tex);
+    animSet.right.setSpriteSheet(tex);
+    animSet.left.setSpriteSheet(tex);
+}
 void Hero::setupAnimations(sf::Texture& texture) {
 
 // ------------------------------ WALK ANIMATIONS -----------------------------
 
+    CostumeSet normal;
     CostumeSet rocket;
-    rocket.walk.up.setSpriteSheet(texture);
+    
+    setSpriteSheet(rocket.idle, texture);
+    setSpriteSheet(rocket.walk, texture);
+
+    setSpriteSheet(normal.idle, texture);
+    setSpriteSheet(normal.walk, texture);
+
     rocket.walk.up.addFrame(sf::IntRect(16, 407, 17, 26));
     rocket.walk.up.addFrame(sf::IntRect(36, 407, 17, 23));
     rocket.walk.up.addFrame(sf::IntRect(56, 407, 17, 26));
     rocket.walk.up.addFrame(sf::IntRect(36, 407, 17, 23));
 
-    rocket.walk.left.setSpriteSheet(texture);
     rocket.walk.left.addFrame(sf::IntRect(11, 378, 20, 24));
     rocket.walk.left.addFrame(sf::IntRect(34, 378, 20, 24));
     rocket.walk.left.addFrame(sf::IntRect(57, 378, 20, 25));
     rocket.walk.left.addFrame(sf::IntRect(34, 378, 20, 24));
 
-    rocket.walk.right.setSpriteSheet(texture);
     rocket.walk.right.addFrame(sf::IntRect(57, 435, 20, 25));
     rocket.walk.right.addFrame(sf::IntRect(34, 435, 20, 24));
     rocket.walk.right.addFrame(sf::IntRect(11, 435, 20, 24));
     rocket.walk.right.addFrame(sf::IntRect(34, 435, 20, 24));
 
-    rocket.walk.down.setSpriteSheet(texture);
-    rocket.walk.down.addFrame(sf::IntRect(36, 350, 17, 24));
     rocket.walk.down.addFrame(sf::IntRect(17, 350, 17, 26));
     rocket.walk.down.addFrame(sf::IntRect(36, 350, 17, 24));
     rocket.walk.down.addFrame(sf::IntRect(55, 350, 17, 26));
     rocket.walk.down.addFrame(sf::IntRect(36, 350, 17, 24));
-    rocket.walk.down.addFrame(sf::IntRect(17, 350, 17, 26));
+
+    normal.walk.up.addFrame(sf::IntRect(6, 61, 17, 25));
+    normal.walk.up.addFrame(sf::IntRect(27, 61, 17, 23));
+    normal.walk.up.addFrame(sf::IntRect(49, 61, 17, 25));
+    normal.walk.up.addFrame(sf::IntRect(27, 61, 17, 23));
+
+    normal.walk.down.addFrame(sf::IntRect(7, 5, 17, 25));
+    normal.walk.down.addFrame(sf::IntRect(26, 5, 17, 23));
+    normal.walk.down.addFrame(sf::IntRect(45, 5, 17, 25));
+    normal.walk.down.addFrame(sf::IntRect(26, 5, 17, 23));
+
+    normal.walk.right.addFrame(sf::IntRect(47, 89, 19, 25));
+    normal.walk.right.addFrame(sf::IntRect(26, 89, 19, 23));
+    normal.walk.right.addFrame(sf::IntRect(5, 89, 19, 23));
+    normal.walk.right.addFrame(sf::IntRect(26, 89, 19, 23));
+
+    normal.walk.left.addFrame(sf::IntRect(5, 33, 19, 24));
+    normal.walk.left.addFrame(sf::IntRect(26, 34, 19, 23));
+    normal.walk.left.addFrame(sf::IntRect(47, 34, 19, 23));
+    normal.walk.left.addFrame(sf::IntRect(26, 34, 19, 23));
     
 // ------------------------------ IDLE ANIMATIONS -----------------------------
 
-    rocket.idle.up.setSpriteSheet(texture);
-    rocket.idle.down.setSpriteSheet(texture);
-    rocket.idle.right.setSpriteSheet(texture);
-    rocket.idle.left.setSpriteSheet(texture);
 
     rocket.idle.up.addFrame(sf::IntRect(36, 407, 17, 23));
     rocket.idle.down.addFrame(sf::IntRect(36, 350, 17, 24));
     rocket.idle.right.addFrame(sf::IntRect(34, 435, 20, 24));
     rocket.idle.left.addFrame(sf::IntRect(34, 378, 20, 24));
 
+    normal.idle.up.addFrame(sf::IntRect(27, 61, 17, 23)); 
+    normal.idle.down.addFrame(sf::IntRect(26, 5, 17, 23)); 
+    normal.idle.right.addFrame(sf::IntRect(26, 89, 19, 23)); 
+    normal.idle.left.addFrame(sf::IntRect(26, 34, 19, 23)); 
+
 // ---------------------------- BICYCLE ANIMATIONS ----------------------------
 
 // ---------------------------- RUNNING ANIMATIONS ----------------------------
 
+    costumeSet.push_back(normal);
     costumeSet.push_back(rocket);
-
-    
 
     costumeIndex = 0;
 
